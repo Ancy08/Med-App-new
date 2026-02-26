5b869fa5e07801b95304543c5561ad2a62f6047a
Ancy08 <ancymaria6023@gmail.com>
2026-02-26 13:34:16 +0530

    cors error fixed

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,14 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors")
 
 const medicineRoutes = require("./routes/medicineRoutes");
 const patientRoutes = require("./routes/patientRoutes");
 const caretakerRoutes = require("./routes/caretakerRoutes");
 
 const app = express();
-
+const allowedOrigins = ["http://localhost:3000","https://med-app-new.vercel.app"];
+app.use(cors({
+origin:function(origin, callback){
+    if(!origin)return callback(null,true);
+    if(allowedOrigins.includes(origin)){
+        return callback(null, true);
+    }
+    else{
+        return callback(new Error("Not allowed by CORS"))
+    }
+}
+}))
 app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=> console.log("Mongo Connected"))
